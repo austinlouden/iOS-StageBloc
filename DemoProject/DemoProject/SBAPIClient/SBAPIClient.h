312d974723e7f6a448764b7e6fb79d21268480cd
Austin Louden <austinlouden@gmail.com>
2013-08-01 15:17:41 -0500

    readd SBAPIClient folder

---
+++ DemoProject/DemoProject/SBAPIClient/SBAPIClient.h
@@ -0,0 +1,20 @@
+//
+//  SBHTTPClient.h
+//  DemoProject
+//
+//  Created by Austin Louden on 7/30/13.
+//  Copyright (c) 2013 StageBloc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "AFHTTPClient.h"
+#import "AFOAuth2Client.h"
+
+@interface SBAPIClient : AFHTTPClient
++ (SBAPIClient *)sharedClient;
+- (BOOL)isAuthorized;
+- (void)authenicateWithUsername:(NSString*)username
+                       password:(NSString*)password
+                        success:(void (^)(AFOAuthCredential *credential))success
+                        failure:(void (^)(NSError *error))failure;
+@end
---
+++ DemoProject/DemoProject/SBAPIClient/SBAPIClient.m
@@ -0,0 +1,87 @@
+//
+//  SBHTTPClient.m
+//  DemoProject
+//
+//  Created by Austin Louden on 7/30/13.
+//  Copyright (c) 2013 StageBloc. All rights reserved.
+//
+
+#import "SBAPIClient.h"
+#import "AFJSONRequestOperation.h"
+#import "AFOAuth2Client.h"
+
+// production keys
+// client id: 0351fbad394c8f0b6a383152a29c812b
+// client secret: 8e83be9ff183046f9aa14f8ffe8604e3
+
+//development keys
+// client id: 28d5a7bdff3f3de38387de64ee0f5947
+// client secret: ec1e0919c732f228066e7ca0a36e4d18
+
+//#error Enter your StageBloc API credentials below, then comment out this line
+static NSString * const kStageBlocConsumerKey = @"0351fbad394c8f0b6a383152a29c812b";
+static NSString * const kStageBlocConsumerSecret = @"8e83be9ff183046f9aa14f8ffe8604e3";
+
+static NSString * const kAFAppDotNetAPIBaseURLString = @"http://api.stagebloc.com/3.0/";
+
+@implementation SBAPIClient
+
++ (SBAPIClient *)sharedClient {
+    static SBAPIClient *_sharedClient = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        _sharedClient = [[SBAPIClient alloc] initWithBaseURL:[NSURL URLWithString:kAFAppDotNetAPIBaseURLString]];
+    });
+    
+    return _sharedClient;
+}
+
+- (id)initWithBaseURL:(NSURL *)url {
+    self = [super initWithBaseURL:url];
+    if (!self) {
+        return nil;
+    }
+    
+    [self registerHTTPOperationClass:[AFJSONRequestOperation class]];
+    
+    // Accept HTTP Header; see http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.1
+	[self setDefaultHeader:@"Accept" value:@"application/json"];
+    
+    return self;
+}
+
+- (BOOL)isAuthorized
+{
+    if([AFOAuthCredential retrieveCredentialWithIdentifier:@"api.stagebloc.com"].accessToken) {
+        return YES;
+    }
+    
+    return NO;
+}
+
+- (void)authenicateWithUsername:(NSString*)username
+                       password:(NSString*)password
+                        success:(void (^)(AFOAuthCredential *credential))success
+                        failure:(void (^)(NSError *error))failure
+{
+    
+    AFOAuth2Client *oauth2Client = [[AFOAuth2Client alloc] initWithBaseURL:[NSURL URLWithString:kAFAppDotNetAPIBaseURLString]
+                                                                  clientID:kStageBlocConsumerKey
+                                                                    secret:kStageBlocConsumerSecret];
+    
+    [oauth2Client authenticateUsingOAuthWithPath:@"oauth2/token/"
+                                        username:username password:password
+                                           scope:@"non-expiring"
+                                         success:^(AFOAuthCredential *credential) {
+        
+        [AFOAuthCredential storeCredential:credential withIdentifier:oauth2Client.serviceProviderIdentifier];
+        [self setAuthorizationHeaderWithToken:[NSString stringWithFormat:@"%@", credential.accessToken]];
+        success(credential);
+        
+    } failure:^(NSError *error) {
+        failure(error);
+    }];
+    
+}
+
+@end
